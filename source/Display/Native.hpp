1228f06973a0384688687b1121afbe4fbe5b874a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-24 15:21:28 +1300

    Add native header.

---
+++ source/Display/Native.hpp
@@ -0,0 +1,12 @@
+//
+//  Native.hpp
+//  This file is part of the "Display Cocoa" project and released under the MIT License.
+//
+//  Created by Samuel Williams on 24/2/2019.
+//  Copyright, 2019, by Samuel Williams. All rights reserved.
+//
+
+#pragma once
+
+#include "Cocoa/Application.hpp"
+#include "Cocoa/Window.hpp"
